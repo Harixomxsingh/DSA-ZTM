d630857e70d563a044ede346264b108e8d29ebfa
Hari om singh <www.hariom03.com@gmail.com>
2022-08-01 15:48:05 +0530

    add o(n) main.js

--- BIG 0/O(N)/main.js
+++ BIG 0/O(N)/main.js
@@ -1,1 +1,12 @@
-console.log("hello o(n)");
+// O(n)
+let arr = ["mary", "john", "krish", "sky", "steve", "otis", "ruby", "peter"];
+
+function findOtis(arr) {
+  // the bigger of arr.length the bigger time take this function that why this is o(n)
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "otis") {
+      console.log(`otis found in the index of ${i}`);
+    }
+  }
+}
+findOtis(arr);

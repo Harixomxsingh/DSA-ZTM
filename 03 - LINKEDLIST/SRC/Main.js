9160be669443d7f8af3d982870609c60d1812127
Hari om Singh <www.hariom03.com@gmail.com>
2022-08-25 22:33:28 +0530

    git create linkedlist class and asign basic constructor

---
+++ 03 - LINKEDLIST/SRC/Main.js
@@ -0,0 +1,15 @@
+// singlyLinkedList
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+let list = new LinkedList(10);
+console.log(list);

f9f3c75c43fd88060bce5f84478a8b6f955d3121
Hari om Singh <www.hariom03.com@gmail.com>
2022-08-25 22:39:39 +0530

    create append method in our linked list

--- 03 - LINKEDLIST/SRC/Main.js
+++ 03 - LINKEDLIST/SRC/Main.js
@@ -9,7 +9,21 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+  // append method
+  append(value) {
+    let newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    // return the linkedlist
+    return this;
+  }
 }
 
 let list = new LinkedList(10);
+list.append(11);
+list.append(12);
 console.log(list);
